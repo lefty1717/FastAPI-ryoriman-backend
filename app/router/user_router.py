d0916e2431f636dbaa2299294d0ad874787738c9
lefty <hank1717@gmail.com>
2023-08-20 17:53:21 +0800

    feat: edit fridge item

--- app/router/user_router.py
+++ app/router/user_router.py
@@ -40,7 +40,22 @@ async def add_fridge_item(user_id: str, fridge_item: Fridge):
         return {"message": "Fridge item added successfully"}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-    
+@router.put("/update_fridge/{user_id}/{id}")
+async def update_fridge_item(user_id: str, id: str, fridge_item: Fridge):
+    food = fridge_item.model_dump()
+    try:
+        result = connect.users.users.update_one(
+            {"_id": ObjectId(user_id), "fridge.id": id},
+            {"$set": {"fridge.$": food}}
+        )
+
+        if result.matched_count == 0:
+            raise HTTPException(status_code=404, detail="User not found")
+        
+        return {"message": "Fridge item updated successfully"}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 @router.delete("/delete_fridge/{user_id}/{id}")
 async def delete_fridge_item(user_id: str, id: str):
     try:
